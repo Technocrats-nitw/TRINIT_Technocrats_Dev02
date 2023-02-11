638c0d7e13544fd74b550c4d3a126b84945ea13f
Ashiqa Rahman <ashiqa@Ashiqas-MacBook-Air.local>
2023-02-11 23:26:15 +0530

    initial commit

---
+++ popup/popup.js
@@ -0,0 +1,232 @@
+const defaultLocation = 'regionOther';
+let userLocation = defaultLocation;
+
+const defaultCarbonIntensityFactorIngCO2PerKWh = 519;
+const kWhPerByteDataCenter = 0.000000000072;
+const kWhPerByteNetwork = 0.000000000152;
+const kWhPerMinuteDevice = 0.00021;
+
+
+const carbonIntensityFactorIngCO2PerKWh = {
+  'regionEuropeanUnion': 276,
+  'regionFrance': 34.8,
+  'regionUnitedStates': 493,
+  'regionChina': 681,
+  'regionOther': defaultCarbonIntensityFactorIngCO2PerKWh
+};
+
+let statsInterval;
+let pieChart;
+
+parseStats = (_dbname) => {
+  const stats = localStorage.getItem(_dbname);
+  return null === stats ? {} : JSON.parse(stats);
+}
+
+getStats = () => {
+  const stats = parseStats('stats');
+  let total = 0;
+  const sortedStats = [];
+
+  for (let origin in stats) {
+    total += stats[origin];
+    sortedStats.push({ 'origin': origin, 'byte': stats[origin] });
+  }
+
+  sortedStats.sort(function(a, b) {
+    return a.byte < b.byte ? 1 : a.byte > b.byte ? -1 : 0
+  });
+
+  const highestStats = sortedStats.slice(0, 4);
+  let subtotal = 0;
+  for (let index in highestStats) {
+    subtotal += highestStats[index].byte;
+  }
+
+  if (total > 0) {
+    const remaining = total - subtotal;
+    if (remaining > 0) {
+      highestStats.push({'origin': 'Others', 'byte': remaining});
+    }
+
+    highestStats.forEach(function (item) {
+      item.percent = Math.round(100 * item.byte / total)
+    });
+  }
+
+  return {
+    'total': total,
+    'highestStats': highestStats
+  }
+}
+
+toMegaByte = (value) => (Math.round(value/1024/1024));
+
+showStats = () => {
+  const stats = getStats();
+
+  if (stats.total === 0) {
+    return;
+  }
+
+  show(statsElement);
+  const labels = [];
+  const series = [];
+
+  const statsListItemsElement = document.getElementById('statsListItems');
+  while (statsListItemsElement.firstChild) {
+    statsListItemsElement.removeChild(statsListItemsElement.firstChild);
+  }
+
+  for (let index in stats.highestStats) {
+    if (stats.highestStats[index].percent < 1) {
+      continue;
+    }
+
+    labels.push(stats.highestStats[index].origin);
+    series.push(stats.highestStats[index].percent);
+    const text = document.createTextNode(`${stats.highestStats[index].percent}% ${stats.highestStats[index].origin}`);
+    const li = document.createElement("LI");
+    li.appendChild(text);
+    statsListItemsElement.appendChild(li);
+  }
+
+  let duration = localStorage.getItem('duration');
+  duration = null === duration ? 0 : duration;
+
+  const kWhDataCenterTotal = stats.total * kWhPerByteDataCenter;
+  const GESDataCenterTotal = kWhDataCenterTotal * defaultCarbonIntensityFactorIngCO2PerKWh;
+
+  const kWhNetworkTotal = stats.total * kWhPerByteNetwork;
+  const GESNetworkTotal = kWhNetworkTotal * defaultCarbonIntensityFactorIngCO2PerKWh;
+
+  const kWhDeviceTotal = duration * kWhPerMinuteDevice;
+  const GESDeviceTotal = kWhDeviceTotal * carbonIntensityFactorIngCO2PerKWh[userLocation];
+
+  const kWhTotal = Math.round(1000 * (kWhDataCenterTotal + kWhNetworkTotal + kWhDeviceTotal)) / 1000;
+  const gCO2Total = Math.round(GESDataCenterTotal + GESNetworkTotal + GESDeviceTotal);
+
+
+  if (!pieChart) {
+    pieChart = new Chartist.Pie('.ct-chart', {labels, series}, {
+      donut: true,
+      donutWidth: 60,
+      donutSolid: true,
+      startAngle: 270,
+      showLabel: true
+    });
+  } else {
+    pieChart.update({labels, series});
+  }
+
+  const megaByteTotal = toMegaByte(stats.total);
+  const kWhTotalStr = kWhTotal.toString();
+  const time = duration.toString();
+  const gCO2TotalStr = gCO2Total.toString();
+  
+  document.getElementById('duration').textContent = time;
+  localStorage.setItem('duration', time);
+
+  document.getElementById('mbTotalValue').textContent = megaByteTotal;
+  localStorage.setItem('mbTotalValue', megaByteTotal);
+
+  document.getElementById('kWhTotalValue').textContent = kWhTotalStr; 
+  localStorage.setItem('kWhTotalValue', kWhTotalStr);
+
+  document.getElementById('gCO2Value').textContent = gCO2TotalStr;
+  localStorage.setItem('gCO2Value', gCO2TotalStr);
+  
+  const equivalenceTitle = document.getElementById('equivalenceTitle');
+  while (equivalenceTitle.firstChild) {
+    equivalenceTitle.removeChild(equivalenceTitle.firstChild);
+  }
+  equivalenceTitle.appendChild(document.createTextNode(chrome.i18n.getMessage('equivalenceTitle', [duration.toString(), megaByteTotal, kWhTotal.toString(), gCO2Total.toString()])));
+}
+
+start = () => {
+  chrome.runtime.sendMessage({ action: 'start' });
+
+  hide(startButton);
+  show(stopButton);
+  show(analysisInProgressMessage);
+  localStorage.setItem('analysisStarted', '1');
+}
+
+stop = () => {
+  chrome.runtime.sendMessage({ action: 'stop' });
+
+  hide(stopButton);
+  show(startButton);
+  hide(analysisInProgressMessage);
+  clearInterval(statsInterval);
+  localStorage.removeItem('analysisStarted');
+}
+
+reset = () => {
+  if (!confirm('Are you sure you want to reset?')) {
+    return;
+  }
+
+  localStorage.removeItem('stats');
+  localStorage.removeItem('duration');
+  hide(statsElement);
+  showStats();
+  hide(resetButton);
+}
+
+init = () => {
+
+  if (null === localStorage.getItem('stats')) {
+    hide(resetButton);
+  } else {
+    show(resetButton);
+  }
+
+  showStats();
+
+  if (null === localStorage.getItem('analysisStarted')) {
+    return;
+  }
+
+  start();
+  statsInterval = setInterval(showStats, 2000);
+}
+
+
+translate = (translationKey) => {
+  return chrome.i18n.getMessage(translationKey);
+}
+
+translateText = (target, translationKey) => {
+  target.appendChild(document.createTextNode(translate(translationKey)));
+}
+
+translateHref = (target, translationKey) => {
+  target.href = chrome.i18n.getMessage(translationKey);
+}
+
+hide = element => element.classList.add('hidden');
+show = element => element.classList.remove('hidden');
+
+const analysisInProgressMessage = document.getElementById('analysisInProgressMessage');
+
+const statsElement = document.getElementById('stats');
+
+const startButton = document.getElementById('startButton');
+startButton.addEventListener('click', start);
+
+const stopButton = document.getElementById('stopButton');
+stopButton.addEventListener('click', stop);
+
+const resetButton = document.getElementById('resetButton');
+resetButton.addEventListener('click', reset);
+
+document.querySelectorAll('[translate]').forEach(function(element) {
+  translateText(element, element.getAttribute('translate'));
+});
+
+document.querySelectorAll('[translate-href]').forEach(function(element) {
+  translateHref(element, element.getAttribute('translate-href'));
+});
+
+init();
